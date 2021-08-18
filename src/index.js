fc465a06ced0ef9877b0f055d6213618783b30b1
wangsf <wangsf@shinemo.com>
2021-08-18 20:53:44 +0800

    update5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 // Step 1: Function createElement
 
 // 例子 实现这样的功能
@@ -47,21 +47,52 @@ function createDom(fiber) {
         ? document.createTextNode('')
         : document.createElement(fiber.type);
         
-    const isProperty = key => key !== 'children'
-    Object.keys(fiber.props)
-        .filter(isProperty)
-        .forEach(name => {
-            dom[name] = fiber.props[name]
-        })
+        updateDom(dom, {}, fiber.props)
 
     return dom;
-    // element.props.children.forEach(child => {
-    //     render(child, dom);
-    // })
-    // container.appendChild(dom);
+}
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+    // Remove old or changed event listeners
+    Object.keys(prevProps)
+        .filter(isEvent)
+        .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2)
+            dom.removeEventListener(eventType, prevProps[name])
+        })
+
+    // Remove old properties
+    Object.keys(prevProps)
+        .filter(isProperty)
+        .filter(isGone(prevProps, nextProps))
+        .forEach(name => {
+            dom[name] = ''
+        })
+
+    // Set new or changed properties
+    Object.keys(nextProps)
+        .filter(isProperty)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => {
+            dom[name] = nextProps[name]
+        })
+    
+    // Add event listeners
+    Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+        const eventType = name.toLowerCase().substring(2)
+        dom.addEventListener(eventType, nextProps[name])
+    })
 }
 
 function commitRoot() {
+    deletions.forEach(commitWork)
     commitWork(wipRoot.child)
     currentRoot = wipRoot
     wipRoot = null
@@ -72,7 +103,13 @@ function commitWork(fiber) {
         return
     }
     const domParent = fiber.parent.dom;
-    domParent.appendChild(fiber.dom);
+    if (fiber.effectTag === 'PLACEMENT' || fiber.dom != null) {
+        domParent.appendChild(fiber.dom)
+    } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
+        updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+    } else if (fiber.effectTag === 'DELETION') {
+        domParent.removeChild(fiber.dom)
+    }
     commitWork(fiber.children);
     commitWork(fiber.sibling);
 }
@@ -86,8 +123,9 @@ function render(element, container) {
         props: {
             children: [element]
         },
-        alternate: cuurntRoot
+        alternate: currentRoot
     }
+    deletions = [];
     nextUnitOfWork = wipRoot;
 }
 
@@ -95,6 +133,7 @@ function render(element, container) {
 let nextUnitOfWork = null;
 let currentRoot = null;
 let wipRoot = null;
+let deletions = null;
 
 function workLoop(deadline) {
     let shouldYield = false;
@@ -102,7 +141,10 @@ function workLoop(deadline) {
         nextUnitOfWork = performUnitOfWork(
             nextUnitOfWork
         )
-        shouldYield = deadline.timeRemaing() < 1
+        shouldYield = deadline.timeRemaining() < 1
+    }
+    if(!nextUnitOfWork && wipRoot) {
+        commitRoot()
     }
     requestIdleCallback(workLoop);
 }
@@ -130,17 +172,48 @@ function performUnitOfWork(fiber) {
 
 function reconcileChildren(wipFiber, elements) {
     let index = 0;
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
     let prevSibling = null;
-    while (index < elements.length) {
+
+    while (index < elements.length || oldFiber != null) {
         const element = elements[index];
-        const newFiber = {
-            type: element.type,
-            props: element.props,
-            parent: fiber,
-            dom: null
+        let newFiber = null;
+        const sameType = oldFiber && element && element.type === oldFiber.type;
+
+        if (sameType) {
+            // update the node
+            newFiber = {
+                type: oldFiber.type,
+                props: element.props,
+                dom: oldFiber.dom,
+                parent: wipFiber,
+                alternate: oldFiber,
+                effectTag: 'UPDATE'
+            }
         }
+        if (element && !sameType) {
+            // add this node
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                dom: null,
+                parent: wipFiber,
+                alternate: null,
+                effectTag: 'PLACEMENT'
+            }
+        }
+        if (oldFiber && !sameType) {
+            // delete the oldFiber's node
+            oldFiber.effectTag = 'DELETEION'
+            deletions.push(oldFiber)
+        }
+
+        if (oldFiber) {
+            oldFiber = oldFiber.sibling
+        }
+
         if (index === 0) {
-            fiber.child = newFiber;
+            wipFiber.child = newFiber;
         } else {
             prevSibling.sibling = newFiber;
         }
@@ -151,19 +224,24 @@ function reconcileChildren(wipFiber, elements) {
 }
 
 
-
+/** @jsx Didact.createElement */
 const Didact = {
     createElement,
     render
 }
 
-/** @jsx Didact.createElement */
-const element = (
-    <div style="background: salmon">
-        <h1>Hello World</h1>
-        <h2 style="text-align:right">from Didact</h2>
-    </div>
-);
-
-const container = document.getElementById('root');
-Didact.render(element, container);
+const updateValue = e => {
+    rerender(e.target.value)
+}
+const container = document.getElementById("root")
+const rerender = value => {
+    const element = (
+      <div>
+        <input onInput={updateValue} value={value} />
+        <h2>Hello {value}</h2>
+      </div>
+    )
+    Didact.render(element, container)
+}
+  
+rerender("World")
